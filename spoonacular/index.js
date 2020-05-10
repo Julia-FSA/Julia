fe39ccdde2540a8c8aa86aeac41a99cd4a8a9f71
Steven Hejnas <shejnas91@gmail.com>
2020-05-10 15:03:14 -0400

    Api call response formatting done.

--- spoonacular/index.js
+++ spoonacular/index.js
@@ -1,8 +1,24 @@
 // const Alexa = require('ask-sdk-core');
 const axios = require('axios')
 const {SpoonacularAPIKey} = require('../secrets.js')
-//let recipe = {};
-// let ingredientArr = [];
+
+let ingr = {
+  id: 0,
+  aisle: 'default',
+  name: 'default',
+  imageURL: 'defaultURL',
+  unit: 'peice'
+}
+let rec = {
+  id: 0,
+  ingredients: [],
+  readyInMinutes: 0,
+  servings: 0,
+  steps: [],
+  title: '',
+  vegan: false,
+  vegetarian: false
+}
 
 const getFromSpoon = (caseType, id, ingredients, name) => {
   if (caseType === 'ingredientByName') {
@@ -11,7 +27,12 @@ const getFromSpoon = (caseType, id, ingredients, name) => {
         `https://api.spoonacular.com/food/ingredients/autocomplete?query=${name}&metaInformation=true&number=1&apiKey=${SpoonacularAPIKey}`
       )
       .then(ingredient => {
-        console.log('ingredient is ', ingredient)
+        ingr.id = ingredient.data[0].id
+        ingr.aisle = ingredient.data[0].aisle
+        ingr.name = ingredient.data[0].name
+        ingr.imageURL = ingredient.data[0].image
+        ingr.unit = ingredient.data[0].possibleUnits
+        console.log('ingredient is ', ingr)
       })
   }
 
@@ -20,8 +41,14 @@ const getFromSpoon = (caseType, id, ingredients, name) => {
       .get(
         `https://api.spoonacular.com/food/ingredients/${id}/information?amount=1&apiKey=${SpoonacularAPIKey}`
       )
+
       .then(ingredient => {
-        console.log('ingredient is ', ingredient.data)
+        ingr.id = ingredient.data.id
+        ingr.aisle = ingredient.data.aisle
+        ingr.name = ingredient.data.name
+        ingr.imageURL = ingredient.data.image
+        ingr.unit = ingredient.data.possibleUnits
+        console.log('ingredient is ', ingr)
       })
   }
 
@@ -36,9 +63,27 @@ const getFromSpoon = (caseType, id, ingredients, name) => {
         `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredientStr}&addRecipeInformation=true&instructionsRequired=true&number=1&apiKey=${SpoonacularAPIKey}`
       )
       .then(recipe => {
-        let resultObj = recipe.data.results[0].analyzedInstructions[0].steps[0]
-
-        console.log('recipe is ', resultObj)
+        let stepArr = [],
+          ingrArr = []
+        let resultObj = recipe.data.results[0]
+        rec.id = resultObj.id
+        rec.title = resultObj.title
+        rec.readyInMinutes = resultObj.readyInMinutes
+        rec.servings = resultObj.servings
+        rec.vegan = resultObj.vegan
+        rec.vegetarian = resultObj.vegetarian
+        // rec.steps = resultObj.analyzedInstructions[0].steps;
+        resultObj.analyzedInstructions[0].steps.forEach(step => {
+          stepArr.push(`Step ${step.number}: ${step.step}`)
+          step.ingredients.forEach(ingredient => {
+            if (!ingrArr.includes(ingredient.id)) {
+              ingrArr.push(ingredient.id, `${ingredient.name}`)
+            }
+          })
+        })
+        rec.ingredients = ingrArr
+        rec.steps = stepArr
+        console.log('recipe is ', rec)
       })
   }
 
@@ -48,22 +93,49 @@ const getFromSpoon = (caseType, id, ingredients, name) => {
         `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&amount=1&apiKey=${SpoonacularAPIKey}`
       )
       .then(recipe => {
-        console.log('recipe is ', recipe.data)
+        let stepArr = [],
+          ingrArr = []
+        let resultObj = recipe.data
+        rec.id = resultObj.id
+        rec.title = resultObj.title
+        rec.readyInMinutes = resultObj.readyInMinutes
+        rec.servings = resultObj.servings
+        rec.vegan = resultObj.vegan
+        rec.vegetarian = resultObj.vegetarian
+        // rec.steps = resultObj.analyzedInstructions[0].steps;
+        resultObj.analyzedInstructions[0].steps.forEach(step => {
+          stepArr.push(`Step ${step.number}: ${step.step}`)
+          step.ingredients.forEach(ingredient => {
+            if (!ingrArr.includes(ingredient.id)) {
+              ingrArr.push(ingredient.id, `${ingredient.name}`)
+            }
+          })
+        })
+        rec.ingredients = ingrArr
+        rec.steps = stepArr
+        console.log('recipe is ', rec)
       })
   }
 }
-//getFromSpoon('ingredientByName', 0, [], 'apple')
-//getFromSpoon('ingredientById', 9266, [], null)
-//getFromSpoon('findByIngredients', 0, [ 'chicken', 'tortilla'], null)
-getFromSpoon('recipeById', 531683, [], null)
+getFromSpoon('ingredientByName', 0, [], 'apple')
+getFromSpoon('ingredientById', 9266, [], null)
+getFromSpoon('findByIngredients', 0, ['peppers', 'steak'], null)
+getFromSpoon('recipeById', 531686, [], null)
 
-// {  "id": 0,
-// "title": "mac & cheese",
-//   "steps": "step 1. lksdfjlaskdjf; step 2. asdjflakdsjf; step 3. ajsdfajkhsdfkjah ...",
-//     "ingredients": "cheese, chicken breast, garlice, onion, peppers",
-//       "readyInMinutes": 30,
-//         "servings": 4,
-//           "vegetarian": false,
+// switch(callType){
+//   case 'ingredientByName':
+//     require('./ingrByName');
+//     break;
+//   case 'ingredientById':
+//     require('./ingrById');
+//     break;
+//   case 'findByIngredients':
+//     require('./recByIngr');
+//     break;
+//   case 'recipeById':
+//     require('./recById');
+//     break;
+//   default:
+//       console.log('Call to Spoonacular did not work, Please check your callType string')
 
-//            "vegan": false
-//           }
+// }
