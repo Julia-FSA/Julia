1c1b8852c25acca764b727fb99ade6e75a40485a
Steven Hejnas <shejnas91@gmail.com>
2020-05-12 16:31:01 -0400

    get all recipes functions working

--- dynamo/read.js
+++ dynamo/read.js
@@ -5,29 +5,58 @@ const {awsConfig} = require('../secrets')
 AWS.config.update(process.env.AWS_CONFIG || awsConfig)
 let docClient = new AWS.DynamoDB.DocumentClient()
 
-
-let fetchOneByKey = function(tableName, stockId) {
-
-  var params = {
-    TableName: tableName,
-    Key: {
-
-      id: stockId
+const getFullRecipe = async recArr => {
+  try {
+    const keyArr = recArr.map(rec => ({id: rec}))
+    let params = {
+      RequestItems: {
+        recipes: {
+          Keys: keyArr
+        }
+      }
     }
-
+    const data = await docClient.batchGet(params).promise()
+    return data.Responses.recipes
+  } catch (error) {
+    console.error(error)
   }
-  docClient.get(params, function (err, data) {
-    if (err) {
-      console.log(
-        'users::fetchOneByKey::error - ' + JSON.stringify(err, null, 2)
-      )
-    } else {
-      console.log(
-        'users::fetchOneByKey::success - ' + JSON.stringify(data, null, 2)
-      )
-      console.log(data.Item.ingredients)
+}
+const getRecipeUser = async userID => {
+  try {
+    const params = {
+      TableName: 'users',
+      Key: {id: userID}
     }
-  })
+    let data = await docClient.get(params).promise()
+    getFullRecipe(data.Item.saved_recipe_ids)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-fetchOneByKey('stocks', 2)
+getRecipeUser('2')
+// let fetchOneByKey = function(tableName, stockId) {
+
+//   var params = {
+//     TableName: tableName,
+//     Key: {
+
+//       id: stockId
+//     }
+
+//   }
+//   docClient.get(params, function (err, data) {
+//     if (err) {
+//       console.log(
+//         'users::fetchOneByKey::error - ' + JSON.stringify(err, null, 2)
+//       )
+//     } else {
+//       console.log(
+//         'users::fetchOneByKey::success - ' + JSON.stringify(data, null, 2)
+//       )
+//       console.log(data.Item.ingredients)
+//     }
+//   })
+// }
+
+// fetchOneByKey('stocks', 2)
