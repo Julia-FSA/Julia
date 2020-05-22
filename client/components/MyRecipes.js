1c393cac5a942361d45d32b6c1ab75024d82d179
David Cheng <dcheng526@gmail.com>
2020-05-22 16:31:38 -0400

    card style css

--- client/components/MyRecipes.js
+++ client/components/MyRecipes.js
@@ -6,6 +6,8 @@ import {fetchFavoriteRecipes} from '../store/recipes'
 /**
  * COMPONENT
  */
+let sampleImage =
+  'https://lh3.googleusercontent.com/proxy/89dmcyifdFSpMuiGbwRBKdRuEOUG1glka6BvB4Bg0QZj8xHCzYFrPtxQUazJFhF9ziFu-Z79oBBm_DKnnOwukL9R3P22'
 class MyRecipes extends React.Component {
   async componentDidMount() {
     const id = this.props.user.id
@@ -17,30 +19,88 @@ class MyRecipes extends React.Component {
     const favoriteRecipes = Object.values(this.props.favoriteRecipes)
     console.log('favoriteRecipes in render()', favoriteRecipes)
     return (
-      <div>
-        <div>
-          <h1 style={{alignText: 'center'}}>Your Favorite Recipes:</h1>
-        </div>
-        <ul className="recipe-container">
-          <div>
-            {favoriteRecipes.length ? (
-              favoriteRecipes.map(recipe => {
-                return (
-                  <li className="recipe" key={recipe.id}>
-                    <Link className="recipe-link" to={`/recipe/${recipe.id}`}>
-                      <img src={recipe.image} className="recipe-image" />
-                      {`${recipe.title} - Ready In:${
-                        recipe.readyInMinutes
-                      }min. - ${recipe.aggregateLikes} Likes`}
-                    </Link>
-                  </li>
-                )
-              })
-            ) : (
-              <li>No Favorite Recipes</li>
-            )}
+      <div className="outer-cont">
+        <div className="container inner-cont">
+          <div className="fridge-cont">
+            <h1>Your Favorite Recipes:</h1>
+            <div id="flex-card-cont">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={sampleImage}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    Thai Carrot Peanut Noodle Salad
+                  </h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="btn btn-warning">
+                    See More
+                  </a>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={sampleImage}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Recipe Title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="btn btn-warning">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={sampleImage}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-        </ul>
+
+          <ul className="recipe-container">
+            <div>
+              {favoriteRecipes.length ? (
+                favoriteRecipes.map(recipe => {
+                  return (
+                    <li className="recipe" key={recipe.id}>
+                      <Link className="recipe-link" to={`/recipe/${recipe.id}`}>
+                        <img src={sampleImage} className="recipe-image" />
+                        {`${recipe.title} - Ready In:${
+                          recipe.readyInMinutes
+                        }min. - ${recipe.aggregateLikes} Likes`}
+                      </Link>
+                    </li>
+                  )
+                })
+              ) : (
+                <li>No Favorite Recipes</li>
+              )}
+            </div>
+          </ul>
+        </div>
       </div>
     )
   }
